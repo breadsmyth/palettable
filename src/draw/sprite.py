68f73bcc10d049a05bd6eef6af624fdb2f690c15
Amy Starr <amy.starrbright@gmail.com>
2026-04-05 09:24:55 -0600

    I forgot to add draw.sprite

---
+++ src/draw/sprite.py
@@ -0,0 +1,12 @@
+import os
+import pygame
+
+
+IMG_PATH = os.path.join('assets', 'img')
+
+def load(filename):
+    filepath = os.path.join(IMG_PATH, filename)
+    image = pygame.image.load(filepath)
+    image = image.convert_alpha()
+
+    return image
